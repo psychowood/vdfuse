6c2eb34c3a4884da964a9957b9140d18182e2fd4
Giacomo Giustozzi <psychowood@gmail.com>
2013-12-16 16:38:56 +0100

    Edited build to reflect local dependencies

--- macbuild.sh
+++ macbuild.sh
@@ -3,12 +3,18 @@
 # http://serverfault.com/questions/174278/mount-an-vhd-on-mac-os-x
 # Authors: Chealion and janm
 
-
 infile=vdfuse.c
 outfile=vdfuse
-incdir="include/"
 INSTALL_DIR="/Applications/VirtualBox.app/Contents/MacOS"
 CFLAGS="-pipe"
+
+if [ ! -d ${INSTALL_DIR} ]
+then
+    echo Cannot find ${INSTALL_DIR}. Did you install VirtuaBox?
+    exit -1
+fi
+
+
 
 gcc -arch i386 "${infile}" \
          "${INSTALL_DIR}"/VBoxDD.dylib \
@@ -18,7 +24,7 @@ gcc -arch i386 "${infile}" \
          "${INSTALL_DIR}"/VBoxDD2.dylib \
          "${INSTALL_DIR}"/VBoxREM.dylib \
         -o "${outfile}" \
-        -I"${incdir}" -I"/usr/local/include/fuse" \
+        -I"virtualbox/" -I"fuse/include/" \
         -Wl,-rpath,"${INSTALL_DIR}"  \
         -lfuse_ino64  \
         -Wall ${CFLAGS}
